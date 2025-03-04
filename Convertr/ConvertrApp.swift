95f8fb60255c8498dc93673b2debfcbd3a60295b
Hannes Nagel <hannes.nagel@codeunity.de>
2025-03-04 10:53:37 +0100

    Initial Commit

---
+++ Convertr/ConvertrApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertrApp.swift
+//  Convertr
+//
+//  Created by Hannes Nagel on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct ConvertrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConvertrTests/ConvertrTests.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertrTests.swift
+//  ConvertrTests
+//
+//  Created by Hannes Nagel on 3/4/25.
+//
+
+import Testing
+@testable import Convertr
+
+struct ConvertrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
